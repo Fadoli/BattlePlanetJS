c03d4056c16c6724baf390ef135d21dce3ed3a35
Franck <f13@laposte.net>
2020-12-07 00:50:04 +0100

    wip : move game loader logic

--- src/server/gameRegistry.js
+++ src/server/gameRegistry.js
@@ -1,4 +1,5 @@
-
+const fs = require('fs');
+const path = require('path');
 
 /**
 * @typedef {Object} GameData
@@ -15,6 +16,11 @@
 * @property {Object} options
 * @property {number} options.maxPlayer
 */
+
+function requireUncached(module) {
+    delete require.cache[require.resolve(module)];
+    return require(module);
+}
 
 /**
  * @type {Object.<string,GameData>}
@@ -38,5 +44,44 @@ module.exports = {
      */
     createGame(gameOptions) {
         return games[gameOptions.name].server(gameOptions.options);
+    },
+    /**
+     * Loads (or re-load) all element in the directory
+     * @param {*} lobby
+     * @param {GameOptions} gameOptions
+     */
+    init(dir, http) {
+        const files = fs.readdirSync(dir, {withFileTypes: true});
+        //console.log(files);
+        files.forEach((file) => {
+            if ( file.isDirectory() ) {
+                const name = file.name;
+                const dirPath = path.join(dir,name);
+                let config = {};
+                try {
+                    config = JSON.parse(fs.readFileSync(path.join(dirPath,'data.json'), {encoding: 'utf-8'}))
+                } catch (e) {
+                    // ...
+                }
+                let client = '404 not found !';
+                try {
+                    client = fs.readFileSync(path.join(dirPath,'client.js'), {encoding: 'utf-8'});
+                } catch (e) {
+                    // ...
+                }
+
+                /**
+                 * @type GameData
+                 */
+                const gameData = {
+                    name: name,
+                    path: dirPath,
+                    client: client,
+                    server: requireUncached(path.join(dirPath,'server.js')),
+                    ...config
+                };
+                this.registerGame(gameData);
+            }
+        });
     }
 }
